6de82fc945e92dc69341235118625886eb64ed13
Dishu <dishuraj129@gmail.com>
2024-03-06 16:16:33 +0530

    Added rabbitmq config

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ const bodyParser=require('body-parser')
 const {PORT}=require('./config/serverConfig')
 const {sendBasicEmail}=require('./services/email-service')
 const jobs=require('./utils/job')
+const {createChannel}=require('./utils/messageQueue')
 const TicetController=require('./controller/ticket-controller')
 
-const setupAndStartServer=()=>{
+const setupAndStartServer=async()=>{
     const app=express();
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({extended:true}))
+    
+    // const channel=await createChannel()
     app.post('/api/v1/tickets',TicetController.create)
     app.listen(PORT,()=>{
         console.log(`Server started on PORT ${PORT}`)
